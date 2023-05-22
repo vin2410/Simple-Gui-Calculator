46b65ab36773e217706306646f9c9a8ad6398c43
vin2410 <lethanhvinh2410@gmail.com>
2023-05-22 12:20:49 +0700

    first commit

---
+++ calculator.py
@@ -0,0 +1,49 @@
+import strsplit
+
+calculation = ""
+def bracketcheck():
+    global calculation
+    if "(" in calculation:
+        for i in range(10):
+            if strsplit.before(calculation, "(").endswith(str(i)):
+                calculation = strsplit.before(calculation, "(") + "*" \
+                              + str(eval(strsplit.inbetween(calculation, "(",")"))) + strsplit.after(calculation,")")
+        if "(" in calculation and strsplit.before(calculation, "(") == "":
+            calculation = strsplit.after(calculation,"(")
+            calculation = str(eval(strsplit.before(calculation, " )"))) + strsplit.after(calculation,")")
+        bracketcheck()
+
+    else: pass
+
+def _btnClick(key):
+    global calculation
+    calculation = calculation +str(key)
+    return calculation
+
+def _btnEqualsInput():
+    global calculation
+    try:
+        bracketcheck()
+        return[eval(calculation),calculation]
+    except ZeroDivisionError:
+        return  ["r u dumb",""]
+    except:
+        return ["Error",""]
+
+def _backspace():
+    global calculation
+    calculation = calculation[:-1]
+
+def square():
+    global calculation
+    try:
+        int(calculation)
+        squared = eval("{}*{}".format(calculation,calculation))
+        if str(squared).endswith(".0"):squared = str(squared).rstrip(".0")
+        return [squared,calculation + '²']
+    except ValueError:
+        if not calculation == "":
+            return [ "development", "feature still in"]
+        else:
+            return [ "0", ""]
+
